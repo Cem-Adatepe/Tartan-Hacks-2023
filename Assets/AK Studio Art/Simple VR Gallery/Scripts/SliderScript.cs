74610cfb679d63f60608dd725afb06ae2416c9bc
Cem-Adatepe <cem.adatepe@outlook.com>
2023-02-04 09:24:35 -0500

    initia commit

---
+++ Assets/AK Studio Art/Simple VR Gallery/Scripts/SliderScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private TMP_Text _sliderText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider.onValueChanged.AddListener((v) => {
+        _sliderText.text = "Bid Amount: $" + v.ToString("0.00");
+        });
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
